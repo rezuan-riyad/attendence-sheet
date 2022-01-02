2915e27cda1791d64011780213b4c04ab4f373a1
Good Bloke <goodbloke@pop-os.localdomain>
2022-01-02 21:29:36 +0600

    Add Basic Folders

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer(){
+  return(
+    <div>
+      <p>All rights reserved by Attendence Sheet</p>
+    </div>
+  )
+}
---
+++ src/screens/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default function Home() {
+  return(
+    <div>
+      <div>
+        <h1>Attendence Sheet</h1>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
